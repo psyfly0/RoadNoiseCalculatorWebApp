f3079af730dd752a38ea949a4797759c50fe87ad
szaboa <szakos90@gmail.com>
2023-11-24 23:25:38 +0100

    First commit

---
+++ src/main/java/noise/road/RoadNoiseCalculatorWebAppApplication.java
@@ -0,0 +1,13 @@
+package noise.road;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadNoiseCalculatorWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadNoiseCalculatorWebAppApplication.class, args);
+	}
+
+}
